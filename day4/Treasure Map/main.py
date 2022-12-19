80dd452b196a428513a9892a42f7378fe1a32664
nhern56 <69496085+nhern56@users.noreply.github.com>
2022-12-19 14:42:43 -0500

    Update main.py

--- day4/Treasure Map/main.py
+++ day4/Treasure Map/main.py
@@ -5,31 +5,10 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-horizontal = position[0]
-vertical = position[1]
+#horizaontal and vertical
+h = int(position[0])
+v = int(position[1])
 
-if horizontal == "1":
-    if vertical == "1":
-        row1 = ["X","⬜️️","⬜️️"]
-    elif vertical == "2":
-        row2 = ["X","⬜️️","⬜️️"]
-    else:
-        row3 = ["X","⬜️️","⬜️️"]
-
-elif horizontal == "2":
-    if vertical == "1":
-        row1 = ["⬜️","X","⬜️️"]
-    elif vertical == "2":
-        row2 = ["⬜️","X","⬜️️"]
-    else:
-        row3 = ["⬜️","X","⬜️️"]
-
-else:
-    if vertical == "1":
-        row1 = ["⬜️","⬜️️","X"]
-    elif vertical == "2":
-        row2 = ["⬜️","⬜️️","X"]
-    else:
-        row3 = ["⬜️","⬜️️","X"]
+map[h-1][v-1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
