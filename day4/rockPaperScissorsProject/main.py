e14fcf6700d48046a49b7e45a4ea5e45aa6ce7ab
nhern56 <69496085+nhern56@users.noreply.github.com>
2022-12-20 09:27:38 -0500

    Update main.py

--- day4/rockPaperScissorsProject/main.py
+++ day4/rockPaperScissorsProject/main.py
@@ -2,15 +2,6 @@ import random
 
 
 
-input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-
-map = [rock, paper, scissors]
-
-choice = map[input-1]
-
-computer = random.choice(map)
-
-
 rock = '''
     _______
 ---'   ____)
@@ -38,8 +29,37 @@ scissors = '''
 ---.__(___)
 '''
 
+import random
 
-print(choice)
+input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 
-print(computer)
+map = [rock, paper, scissors]
 
+print(map[input])
+
+computer = random.randint(0,2)
+
+print(map[computer])
+
+if input == 0:
+  if computer == 1:
+    print("computer wins")
+  if computer == 2: 
+    print("you win")
+  elif input == computer:
+    print("its a draw")
+elif input == 1:
+  if computer == 0:
+    print("you win")
+  if computer == 2: 
+    print("computer wins")
+  elif input == computer:
+    print("its a draw")
+elif input == 2: 
+  if computer == 0:
+    print("you win")
+  if computer == 1: 
+    print("computer wins")
+  elif input == computer:
+   print("its a draw")
+  
