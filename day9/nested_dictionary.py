16cf4d77ac60e4c00d9dfe5d35b84f271cc39f09
nhern56 <69496085+nhern56@users.noreply.github.com>
2022-12-23 18:29:17 -0500

    Create nested_dictionary.py

---
+++ day9/nested_dictionary.py
@@ -0,0 +1,26 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 
+
+def add_new_country(country, visited, cities):
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visited
+    new_country["cities"] = cities
+    travel_log.append(new_country)
+    
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
